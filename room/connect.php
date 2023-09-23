d0d8723ef7f63932d40ce619a8c762e64a73a6e1
Jpgomera19 <jpgomera19@gmail.com>
2023-09-23 18:54:53 +0800

    SEPTEMBER 23, 2023

--- room/connect.php
+++ room/connect.php
@@ -1,12 +1,12 @@
 <?php
 
-	$db_user="root";
-	$db_password= "";
-	$db_host= "localhost";
-	$db_database= "calendar";
-	$link=mysql_connect($db_host, $db_user, $db_password) or die ("Connection Failure");
+	// $db_user="root";
+	// $db_password= "";
+	// $db_host= "localhost";
+	// $db_database= "calendar";
+	// $link=mysql_connect($db_host, $db_user, $db_password) or die ("Connection Failure");
 	
-	mysql_select_db($db_database, $link);
+	// mysql_select_db($db_database, $link);
 
 
 
